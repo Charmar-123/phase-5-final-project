37de3e46bfae234f5ff9ea5a1a78b52c907a11ea
Charbel Maroun <charbel-maroun@hotmail.com>
2023-08-14 18:23:06 -0400

    add associations and seed data

--- db/seeds.rb
+++ db/seeds.rb
@@ -1,7 +1,18 @@
-# This file should contain all the record creation needed to seed the database with its default values.
-# The data can then be loaded with the bin/rails db:seed command (or created alongside the database with db:setup).
-#
-# Examples:
-#
-#   movies = Movie.create([{ name: 'Star Wars' }, { name: 'Lord of the Rings' }])
-#   Character.create(name: 'Luke', movie: movies.first)
+# db/seeds.rb
+
+# Users
+user1 = User.create(name: 'Alice', bio: 'Fitness enthusiast', email: 'alice@example.com', password_digest: BCrypt::Password.create("1234"))
+user2 = User.create(name: 'Bob', bio: 'Gym addict', email: 'bob@example.com', password_digest: BCrypt::Password.create("1234"))
+
+# Workouts
+workout1 = Workout.create(date: '2023-08-15', time: 60, exercise_type: 'Cardio', intensity: 7)
+workout2 = Workout.create(date: '2023-08-16', time: 45, exercise_type: 'Strength Training', intensity: 8)
+
+# Exercises
+exercise1 = Exercise.create(name: 'Running', sets: '1', reps: '1', weight: '0', target_area: 'Cardio', description: '30-minute jog', workout_id: workout1)
+exercise2 = Exercise.create(name: 'Squats', sets: '3', reps: '10', weight: '50', target_area: 'Legs', description: 'Bodyweight squats', workout_id: workout2)
+
+# Participants
+participant1 = Participant.create(user: user1, workout: workout1, admin: true)
+participant2 = Participant.create(user: user2, workout: workout1, admin: false)
+participant3 = Participant.create(user: user1, workout: workout2, admin: true)
