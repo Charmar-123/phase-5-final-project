207f485c467829a8b36f0c1556f12f0a3d8eebc0
Charbel Maroun <charbel-maroun@hotmail.com>
2023-08-17 17:21:44 -0400

    clean up motion in createworkout

--- client/src/App.js
+++ client/src/App.js
@@ -6,7 +6,7 @@ import AboutUs from './components/AboutUs';
 import WorkoutCard from './components/WorkoutCard';
 import WorkoutPage from './components/WorkoutPage';
 import CreateWorkout from './components/CreateWorkout';
-import RatingsDropDown from './components/ratings/RatingsDropDown';
+
 
 
 
