a336e497a2efd83ba56080239afb776c7b1cddff
Charbel Maroun <charbel-maroun@hotmail.com>
2023-08-18 14:12:24 -0400

    move ratings into own component

--- client/src/components/CreateWorkout.js
+++ client/src/components/CreateWorkout.js
@@ -9,44 +9,14 @@ import KeyboardArrowDownIcon from '@mui/icons-material/KeyboardArrowDown';
 import KeyboardArrowUpIcon from '@mui/icons-material/KeyboardArrowUp';
 
 import ArrowForwardIcon from '@mui/icons-material/ArrowForward';
+import RatingsDropdown from './RatingsDropdown';
 const CreateWorkout = () => {
     const [date, selectedDate] = useState(dayjs().add(5, "minute"))
-    const [isToggled, setIsToggled] = useState(false);
-    const [selectedRating, setSelectedRating] = useState({ text: 'Pick a level out of 5!', rating: '😅' })
-
-    const styles = {
-        ratingsText: {
-
-            textAlign: 'center',
-            fontFamily: 'CardFont',
-            fontWeight: 900
 
 
 
-        },
 
-        ratingsDiv: {
-            boxShadow: "rgba(6, 24, 44, 0.4) 0px 0px 0px 2px, rgba(6, 24, 44, 0.65) 0px 4px 6px -1px, rgba(255, 255, 255, 0.08) 0px 1px 0px inset", width: 210, height: 60, borderRadius: 6, marginBottom: 19
-        }
 
-    }
-    const handleClick = () => {
-        setIsToggled(!isToggled); // Toggle the value using the NOT operator
-    };
-
-    const ratingVariants = {
-        hidden: { opacity: 0 },
-        show: {
-            opacity: 1,
-            transition: {
-                staggerChildren: 0.5
-            }
-        },
-        exit: {
-            opacity: 0,
-        }
-
-    }
     const titleVaritants = {
         hidden: { opacity: 0 },
         show: {
@@ -69,26 +39,15 @@ const CreateWorkout = () => {
         }
     }
 
-    const ratingItem = {
-        hidden: { opacity: 0 },
-        show: {
-            opacity: 1,
 
-        }
 
-    }
-
-    const onRatingClick = (text, rating) => {
-        setSelectedRating({ text: text, rating: rating })
-        handleClick()
-    }
 
     return (
 
         <Box
             sx={{
                 height: 10000,
-                backgroundColor: '#dee2e3',
+                background: "linear-gradient(90deg, rgba(208,208,244,1) 5%, rgba(208,208,244,1) 17%, rgba(0,212,255,1) 50%)",
                 paddingLeft: 5
             }}>
 
@@ -122,11 +81,11 @@ const CreateWorkout = () => {
                         <Typography sx={{ fontFamily: 'CardFont', fontWeight: '950', paddingTop: 4 }} variant='h4'>Pick A Date And Time</Typography>
                         <LocalizationProvider dateAdapter={AdapterDayjs}>
 
-                            <DateTimePicker 
-                            disablePast
-                            label="Pick A Date And Time" 
-                            value={date}
-                            onChange={(value) => console.log(value)}
+                            <DateTimePicker
+                                disablePast
+                                label="Pick A Date And Time"
+                                value={date}
+                                onChange={(value) => console.log(value)}
                             />
 
                         </LocalizationProvider>
@@ -136,126 +95,9 @@ const CreateWorkout = () => {
                         <Typography sx={{ fontFamily: 'CardFont', fontWeight: '950', paddingTop: 4 }} variant='h3'>Tell Us About It!</Typography>
                         <Typography sx={{ fontFamily: 'CardFont', fontWeight: '950', paddingTop: 1 }} variant='h5'>What equipment do we need and what type of workout is it.</Typography>
                     </motion.div>
-                    <motion.div
-                        variants={titleItem}>
-                        <Typography sx={{ fontFamily: 'CardFont', fontWeight: '950', paddingTop: 4 }} variant='h3'>How Intense Is It!</Typography>
 
-                        <Box sx={{ display: 'flex', flexDirection: 'row', alignItems: 'center', justifyContent: 'flex-start' }}>
-                            <Box sx={{ display: 'flex', flexDirection: 'column', alignItems: 'center' }}>
-
-
-                                <motion.div
-                                    onClick={handleClick}
-                                    style={{ ...styles.ratingsDiv, display: 'flex', alignContent: 'flex-start', alignItems: 'center', width: 240 }}
-                                    whileHover={{ scale: 1.2 }} whileTap={{ scale: 0.8 }}>
-
-
-                                    <Typography sx={{ ...styles.ratingsText, flex: '1', textAlign: 'center' }}>{selectedRating.text} <br /> {selectedRating.rating}
-                                    </Typography>
-                                    {isToggled ? <KeyboardArrowUpIcon /> : <KeyboardArrowDownIcon />}
-                                </motion.div>
-
-
-
-
-                                <AnimatePresence initial={false}>
-
-                                    {isToggled &&
-
-                                        <>
-                                            <motion.div
-                                                variants={ratingVariants}
-                                                initial="hidden"
-                                                animate="show"
-                                                exit="exit"
-                                            >
-
-                                                <motion.div
-                                                    variants={ratingItem} whileHover={{ scale: 1.2 }} whileTap={{ scale: 0.8 }}
-                                                    style={styles.ratingsDiv}
-
-                                                    onClick={() => {
-
-                                                        onRatingClick(`Piece of cake!`, `😅`)
-                                                    }}
-                                                >
-                                                    <Typography
-
-                                                        sx={styles.ratingsText}>Piece of cake!<br /> 😅</Typography>
-
-
-                                                </motion.div>
-
-
-
-                                                <motion.div
-
-                                                    onClick={() => {
-
-                                                        onRatingClick(`Feeling the burn!`, `😅😅`)
-                                                    }}
-                                                    variants={ratingItem} whileHover={{ scale: 1.2 }} whileTap={{ scale: 0.8 }}
-                                                    style={styles.ratingsDiv}>
-                                                    <Typography sx={styles.ratingsText}>Feeling the burn!<br />  😅😅</Typography>
-
-                                                </motion.div>
-                                                <motion.div
-
-                                                    onClick={() => {
-
-                                                        onRatingClick(`Sweating buckets!`, `😅😅😅`)
-                                                    }}
-                                                    variants={ratingItem} whileHover={{ scale: 1.2 }} whileTap={{ scale: 0.8 }}
-                                                    style={styles.ratingsDiv}>
-                                                    <Typography sx={styles.ratingsText}>Sweating buckets!<br />  😅😅😅</Typography>
-
-                                                </motion.div>
-                                                <motion.div
-
-                                                    onClick={() => {
-
-                                                        onRatingClick(`Gasping for air!`, `😅😅😅😅`)
-                                                    }}
-                                                    variants={ratingItem} whileHover={{ scale: 1.2 }} whileTap={{ scale: 0.8 }}
-                                                    style={styles.ratingsDiv}>
-                                                    <Typography sx={styles.ratingsText}>Gasping for air!<br />  😅😅😅😅</Typography>
-
-                                                </motion.div>
-                                                <motion.div
-
-                                                    onClick={() => {
-
-                                                        onRatingClick(`Total workout apocalypse!`, `😅😅😅😅😅`)
-                                                    }}
-                                                    variants={ratingItem} whileHover={{ scale: 1.2 }} whileTap={{ scale: 0.8 }}
-                                                    style={styles.ratingsDiv}>
-                                                    <Typography sx={styles.ratingsText}>Total workout apocalypse!<br />  😅😅😅😅😅</Typography>
-
-                                                </motion.div>
-
-
-
-
-                                            </motion.div>
-
-
-                                        </>
-
-
-                                    }
-
-
-                                </AnimatePresence>
-
-
-
-
-
-
-                            </Box>
-                        </Box>
-
-                    </motion.div>
+                    <RatingsDropdown titleItem={titleItem}/>
+                    
                 </Box>
 
 
