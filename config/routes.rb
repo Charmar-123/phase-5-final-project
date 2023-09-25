20599dbf8eb933552490891b160ea149078421fc
Charbel Maroun <charbel-maroun@hotmail.com>
2023-09-25 00:46:48 -0400

    update routes

--- config/routes.rb
+++ config/routes.rb
@@ -1,14 +1,16 @@
 Rails.application.routes.draw do
-  root 'home#index'
-  resources :participants
-  resources :exercises
-  resources :workouts
-  resources :users
+  namespace :api do
+    resources :participants
+    resources :exercises
+    resources :workouts
+    resources :users
+    post '/login', to: 'sessions#create'
+    delete '/logout', to:'sessions#destroy'
+    get '/authorized_user', to: 'users#show'
   # Routing logic: fallback requests for React Router.
   # Leave this here to help deploy your app later!
-  get "*path", to: "fallback#index", constraints: ->(req) { !req.xhr? && req.format.html? }
+  end
+    get "*path", to: "fallback#index", constraints: ->(req) { !req.xhr? && req.format.html? }
 
-  post '/login', to: 'sessions#create'
-  delete '/logout', to:'sessions#destroy'
-  get '/authorized_user', to: 'users#show'
+
 end
